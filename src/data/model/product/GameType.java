e065200315d197f9b8da18f0d13dd45119f3fa0c
Alex Prykhodko <alex.prykh96@gmail.com>
2021-12-19 11:59:38 +0200

    game store

---
+++ src/data/model/product/GameType.java
@@ -0,0 +1,5 @@
+package data.model.product;
+
+public enum GameType {
+    GAME, DLC, SUBSCRIPTION, ADD_ON, IN_GAME_MONEY;
+}
---
+++ src/data/model/user/Role.java
@@ -0,0 +1,5 @@
+package data.model.user;
+
+public enum Role {
+    USER, ADMIN;
+}
